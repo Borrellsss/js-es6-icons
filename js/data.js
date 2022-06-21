799cff3e2d4e94fa0b8a668add7e9087ef1323ac
Borrellsss <edoardofilippo.borrello@gmail.com>
2022-06-21 22:24:30 +0200

    esercizio completo + bonus

--- js/data.js
+++ js/data.js
@@ -13,6 +13,15 @@
 // un tipo dalla select, visualizzare solamente
 // le icone corrispondenti.
 
+// BONUS
+// 1- modificare la struttura dati fornita e valorizzare la
+// proprietà "color" in modo dinamico: generare in modo casuale
+// un codice colore, sapendo che la notazione esadecimale è
+// formata dal simbolo "#" seguito da 6 caratteri
+// alfanumerici compresi tra 0 e 9 e A e F.
+// 2- popolare le options della select della milestone 3 dinamicamente.
+
+// *ARRAYS*
 const iconsArray = [
 	{
 		name: 'cat',
@@ -128,15 +137,121 @@ const iconsArray = [
 	}
 ];
 
-const iconsWrapper = document.getElementById("icons-wrapper");
-
+// *sovrascrivo il colore dell'elemento con il nuovo colore in versione*
+// *esadecimale con le varie sfumature di colore in base al colore di partenza:*
+// *---se il colore di partenza è arancione chiamo la funzione "randomColorGen()" nella posizione*
+// *3 e 4 del colore esadecimale avendo ff nella 1 e 2 posizione e 00 nella 5 e 6*
+// *---se il colore di partenza è green chiamo la funzione "randomColorGen()" nella posizione*
+// *1 e 2 del colore esadecimale avendo ff nella 3 e 4 posizione e 00 nella 5 e 6*
+// *---se il colore di partenza è blue chiamo la funzione "randomColorGen()" nella posizione*
+// *3 e 4 del colore esadecimale avendo ff nella 5 e 6 posizione e 00 nella 1 e 2*
 iconsArray.forEach((element) => {
-	// console.log(element);
-	const thisIcon = `${element.family} ${element.prefix}${element.name}`;
+	if(element.color === "orange") {
+		element.color = `#ff${randomColorGen()}00`;
+	} else if (element.color === "green") {
+		element.color = `#${randomColorGen()}ff00`;
+	} else if (element.color === "blue") {
+		element.color = `#00${randomColorGen()}ff`;
+	}
+});
+// !DEBUG!
+// console.log(iconsArray);
 
-	iconsWrapper.innerHTML += `
-	<span class="icon-${element.color}">
-		<i class="${thisIcon}">
-	</i></span>
-	`;
+const typeArray = [];
+
+// *FUNCTIONS*
+function printIcons(array, wrapper) {
+	array.forEach((element) => {
+		// !DEBUG!
+		// console.log(element);
+		const thisIcon = `${element.family} ${element.prefix}${element.name}`;
+	
+		wrapper.innerHTML += `
+		<span style="color: ${element.color}">
+			<i class="${thisIcon}">
+		</i></span>
+		`;
+	});
+}
+
+// *dichiaro una funzione che genera una stringa contentente due valori randomici*
+// *tra 0 e f e ritorno la stringa salavata in "hexCodePart"*
+function randomColorGen() {
+	const hexCodeValues = "0123456789abcdef";
+	let hexCodePart = "";
+	for(let i = 0; i < 2; i++) {
+		hexCodePart += hexCodeValues[getRndInteger(0, hexCodeValues.length)];
+	}
+
+	return hexCodePart;
+	// !DEBUG!
+	// console.log(hexCodePart);
+}
+
+function getRndInteger(min, max) {
+	return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+// *VARIABLES*
+// *DOM elemnts*
+const iconsWrapper = document.getElementById("icons-wrapper");
+// !DEBUG!
+// console.log(iconsWrapper);
+
+const selectType = document.getElementById("type");
+// !DEBUG!
+// console.log(selectType);
+
+// *other variables*
+const thisSelect = 'all'
+let option = `<option value="${thisSelect}">${thisSelect}</option>`;
+selectType.innerHTML += option;
+
+// *LOGIC*
+// *inizio un ciclo "forEach" e pusho in un array vuoto "typeArray" il valore*
+// *della proprietà "color" di ogni singolo "object" solo se non è*
+// *già incluso nell'array stesso*
+iconsArray.forEach((element) => {
+    if(!typeArray.includes(element.type)) {
+        typeArray.push (element.type);
+    } 
+})
+// !DEBUG!
+// console.log(typeArray);
+
+// *una volta popolato l'array "typeArray" stampo in pagina il template*
+// *con "value" = al singolo elemento dell'array che ho ricavato tramite il ciclo "forEach"*
+typeArray.forEach((element) => {
+    option = `<option value="${element}">${element}</option>`;
+    selectType.innerHTML += option;
+})
+
+// *resetto la value a "all" al caricamento della pagina*
+selectType.value = "all";
+
+// *chiamo la funzione che stampa le icone in pagina*
+printIcons(iconsArray, iconsWrapper);
+
+// *ADD-EVENT-LISTENERS*
+selectType.addEventListener("change", function() {
+	iconsWrapper.innerHTML = "";
+	const thisType = this.value;
+	// !DEBUG!
+	// console.log(thisType);
+
+	if(thisType !== "all") {
+		const filteredArray = iconsArray.filter((element) => {
+			const filteredObject = (element.type === thisType);
+			// !DEBUG!
+			// console.log(filteredObject);
+
+			return filteredObject;
+		});
+		// !DEBUG!
+		// console.log(filteredArray);
+		printIcons(filteredArray, iconsWrapper);
+	} else {
+		printIcons(iconsArray, iconsWrapper);
+	}
+
 });
